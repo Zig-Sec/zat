0e8126aaf784ae7e4cdd100ebeba180f27c9f3ff
David P. Sugar <david@thesugar.de>
2025-09-06 17:31:32 +0200

    Added [EUVD](https://euvd.enisa.europa.eu/) advisory data structure to zat.

--- src/root.zig
+++ src/root.zig
@@ -2,10 +2,12 @@ pub const PackageInfo = @import("PackageInfo.zig");
 pub const InspectBuild = @import("InspectBuild.zig");
 pub const cmd = @import("cmd.zig");
 pub const spdx = @import("spdx.zig");
+pub const vulnerability = @import("vulnerability.zig");
 
 test {
     _ = PackageInfo;
     _ = InspectBuild;
     _ = cmd;
     _ = spdx;
+    _ = vulnerability;
 }
